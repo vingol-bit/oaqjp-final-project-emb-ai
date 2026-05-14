38795ca254d49c43990b1e96a1bed3bbeeeaaa04
vingol <vingol@gmail.com>
2026-05-14 10:00:23 -0400

    impl task6

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 # Import the Flask class from the flask module --->flask --app server --debug run
+from EmotionDetection.emotion_detection import emotion_detector
 from flask import Flask, make_response, request, render_template
+
 
 # Create an instance
 #
@@ -10,6 +12,12 @@ app = Flask(__name__)
 @app.route("/")
 def render_index_page(): 
     return render_template('index.html')
+
+@app.route("/emotionDetector")
+def sent_emotion():
+    text = request.args.get('textToAnalyze') 
+    emotions = emotion_detector(text)
+    return make_response(emotions)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
